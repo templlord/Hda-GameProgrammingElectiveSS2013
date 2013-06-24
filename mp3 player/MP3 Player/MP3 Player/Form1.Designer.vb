755052f0c806c15b058644a633a2a5263c133029
Norbert Bastius <nokio85@gmx.de>
2013-06-25 01:29:47 +0200

    !A test

---
+++ mp3 player/MP3 Player/MP3 Player/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.player = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.opd = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.player, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'player
+        '
+        Me.player.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.player.Enabled = True
+        Me.player.Location = New System.Drawing.Point(-4, 25)
+        Me.player.Name = "player"
+        Me.player.OcxState = CType(resources.GetObject("player.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.player.Size = New System.Drawing.Size(619, 363)
+        Me.player.TabIndex = 0
+        '
+        'opd
+        '
+        Me.opd.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(612, 387)
+        Me.Controls.Add(Me.player)
+        Me.Name = "Form1"
+        Me.Text = "MP3 Player"
+        CType(Me.player, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents player As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents opd As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ mp3 player/MP3 Player/MP3 Player/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+
+    Private Sub opd_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles opd.FileOk
+
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+
+
+        opd.InitialDirectory = "c:\"
+
+        opd.ShowDialog()
+
+        player.URL = opd.FileName
+
+        player.Ctlcontrols.play()
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+        Application.Exit()
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
